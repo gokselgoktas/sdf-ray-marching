a5e2195171f6ea5fbe6345b0119195f58ce35c14
Goksel Goktas <goksel@pixel.local>
2016-05-22 12:12:53 +0300

    Initial implementation with ambient lighting support

---
+++ Assets/Scripts/RayMarcher.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+using System.Collections;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof (Camera))]
+public class RayMarcher : MonoBehaviour
+{
+        [Range(1, 1000)]
+        public int maximumIterationCount = 128;
+
+        [Range(0.0001f, 0.1f)]
+        public float epsilon = 0.0025f;
+
+        private Shader m_Shader;
+        public Shader shader
+        {
+                get
+                {
+                        if (m_Shader == null)
+                                m_Shader = Shader.Find("Hidden/Ray-marcher");
+
+                        return m_Shader;
+                }
+        }
+
+        private Material m_Material;
+        public Material material
+        {
+                get
+                {
+                        if (m_Material == null)
+                        {
+                                if (shader == null || !shader.isSupported)
+                                        return null;
+
+                                m_Material = new Material(shader);
+                        }
+
+                        return m_Material;
+                }
+        }
+
+        private Camera m_Camera;
+        public Camera camera_
+        {
+                get
+                {
+                        if (m_Camera == null)
+                                m_Camera = GetComponent<Camera>();
+
+                        return m_Camera;
+                }
+        }
+
+        private Mesh m_Quad;
+        private Mesh quad
+        {
+                get
+                {
+                        if (m_Quad == null)
+                        {
+                                Vector3[] vertices = new Vector3[4]
+                                {
+                                        new Vector3( 1.0f, 1.0f, 0.0f),
+                                        new Vector3(-1.0f, 1.0f, 0.0f),
+                                        new Vector3(-1.0f,-1.0f, 0.0f),
+                                        new Vector3( 1.0f,-1.0f, 0.0f),
+                                };
+
+                                int[] indices = new int[6] { 0, 1, 2, 2, 3, 0 };
+
+                                m_Quad = new Mesh();
+                                m_Quad.vertices = vertices;
+                                m_Quad.triangles = indices;
+                        }
+
+                        return m_Quad;
+                }
+        }
+
+        private CommandBuffer m_CommandBuffer;
+
+        void OnEnable()
+        {
+                camera_.depthTextureMode = DepthTextureMode.Depth;
+        }
+
+        void OnDisable()
+        {
+                if (camera_ != null)
+                {
+                        if (m_CommandBuffer != null)
+                        {
+                                camera_.RemoveCommandBuffer(CameraEvent.AfterGBuffer, m_CommandBuffer);
+                        }
+
+                        m_CommandBuffer = null;
+                }
+        }
+
+        void OnPreRender()
+        {
+                material.SetInt("_MaximumIterationCount", maximumIterationCount);
+                material.SetFloat("_Epsilon", epsilon);
+
+                if (m_CommandBuffer == null)
+                {
+                        m_CommandBuffer = new CommandBuffer();
+                        m_CommandBuffer.name = "Ray-marcher";
+                        m_CommandBuffer.DrawMesh(quad, Matrix4x4.identity, material, 0, 0, null);
+
+                        camera_.AddCommandBuffer(CameraEvent.AfterGBuffer, m_CommandBuffer);
+                }
+        }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+# Ray Marching Sandbox
+
+A simple project showing how to integrate a signed distance field (SDF) based
+ray marcher into Unity's deferred shading pipeline.
+
+Our implementation fills in the necessary information garnered from the virtual
+scene's SDF into Unity's G-buffer, into which Unity further writes, throughout
+the G-buffer construction part of its deferred shading pipeline.
+
+The result is a seamless composition of our virtual scene and the actual scene
+in Unity.
+
+## License
+
+The author(s) of this software hate viral software licenses (hi, GPL) and
+really annoying stuff like software patents. That's why this entire thing is
+wholly public domain, yo!
+
+Any being (not just humans) is free to copy, modify, publish, use, compile,
+sell or distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any means.
+
+## Contributors
+
+Here's a list of great apes that pressed a few keys on this weird thing called
+a keyboard, or gave some feedback about why shit is going bonkers, and made
+stuff happen on the screen:
+
+* Goksel Goktas (author)
+* Jens Holm (author)
+* ~~Sly~~ Tim Cooper
